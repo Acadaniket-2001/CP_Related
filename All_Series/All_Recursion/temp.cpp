12ac845b0941282669bd084affe2908da4d2d921
Aniket Kumar Gupta <135992322+Acadaniket-2001@users.noreply.github.com>
2024-07-19 11:16:07 +0530

    "AZ premium phase 2 added"

--- All_Series/All_Recursion/temp.cpp
+++ All_Series/All_Recursion/temp.cpp
@@ -1,64 +1,101 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
-#define ll long long
-#define sz(x) ((int)(x.size()))
+using namespace __gnu_pbds;
+template<class T> using pbset=tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update> ;
+template<class T> using pbmultiset=tree<T, null_type, less_equal<T>, rb_tree_tag,tree_order_statistics_node_update> ;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+#define f(i,a,b) for(long long i=a;i<=b;i++)
+#define rf(i,a,b) for(long long i=a;i>=b;i--)
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define P 1000000007
+#define P1 998244353
+#define llINT_MAX 9e18
+#define iINT_MAX 1e9
+#define endl "\n"
 #define pb push_back
 #define ppb pop_back
+#define eb emplace_back
+#define mkp make_pair
 #define ff first
 #define ss second
-#define MOD 1000000007
-#define rep(i, a, b) for(ll i = a; i <= b; i++)
-#define rrep(i, a, b) for(ll i = a; i >= b; i--)
-#define all(x) x.begin(), x.end() 
+#define vll vector<ll>
+#define vvl vector<vll>
+#define p_q priority_queue
+#define pqmax priority_queue<ll>
+#define pqmin priority_queue<ll,vector<ll>,greater<ll>>
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define YES cout << "YES" << "\n"
+#define NO cout << "NO" << "\n"
+#define yes cout << "Yes" << "\n"
+#define no cout << "No" << "\n"
+#define gg cout << -1 << "\n"
+#define ln cout << "\n"
+#define re return
+template <class T> istream& operator >> (istream &is, vector<T> &v) { for(auto &i : v)    is >> i; return is; };
+template <class T> ostream& operator << (ostream &os, vector<T> &v) { for(auto &i : v)    os << i << " "; cout << "\n"; return os; };
+#ifndef ONLINE_JUDGE
+#include <algo/debug.h>
+#else
+#define dbg(...) ;
+#define debug(...) ;
+#define crndl ;
+#endif
+long long POW(long long a,long long b){return (long long)(pow(a,b)+0.5);}
+long long gcd(long long a,long long b){return __gcd(a,b);}
+long long lcm(long long a,long long b){return (a*b)/__gcd(a,b);}
+long long Ceil(long long a,long long b){return (a+b-1)/b;}
+long long Sqrt(long long x){ long long y=sqrt(x)+5;while(y*y>x)y--;return y;}
+long long sq(long long x){return (1ll*x*x);}
+/*-------------------> Current Level <----------------------
+2. Think all possibilities
+3. Think greedy
+4. Think sortings
+5. Think constructive algorithms
+1. ⭐ Think brute force
+6. ⭐ Think implementation
+7. ⭐ Think math
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
-//top_Down_DP
-ll dp[1004][1004];
-ll minSmokeTD(const vector<int>& v, int i, int j) {
-    if(i >= j)
-        return 0;
+void solve()
+{
+    ll n; cin >> n;
+    vll v(n); cin >> v;
 
-    if(dp[i][j] != -1)
-        return dp[i][j];
-
-    ll ans = INT_MAX;
-    rep(k, i, j - 1) {
-        
-        ll l = 0, r = 0;
-
-        rep(x, i, k) {
-            l += v[x];
-            l %= 100;
-        }
-        
-        rep(x, k + 1, j) {
-            r += v[x];
-            r %= 100;
-        }
-
-        ans = min(ans, minSmokeTD(v, i, k) + minSmokeTD(v, k + 1, j) + l*r);
+    vll ans;
+    stack<ll> st;
+    rf(i, n - 1, 0) {
+        while(!st.empty() and v[st.top()] <= v[i]) st.pop();
+        if(st.empty())
+            ans.pb(n);
+        else
+            ans.pb(st.top());
+        st.push(i);
     }
-    return dp[i][j] = ans;
+    
+    reverse(all(ans));
+    cout << ans;
 }
 
-signed main() {
-    
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+// JUST ONE MISTAKE, AND YOU ARE HAHAHAHHHHHHHHHHAAAAAAAAAAAAAAA!!!
+/* Wo sare problems karo jo lage ki ho jayega but nhi ho pata */
 
+int main()
+{
+    fastio();
+    #ifndef ONLINE_JUDGE
+        freopen("io/Error.txt", "w", stderr);
+        // freopen("io/Input.txt", "r", stdin);
+        // freopen("io/Output.txt", "w", stdout);
+    #endif
 
-    int n;
-    while(scanf("%d", &n) != EOF) {
-        // memset(dp, -1, sizeof dp);
-
-        rep(i, 0, n - 1)
-            rep(j, 0, n - 1)
-                dp[i][j] = -1;
-
-        vector<int> v(n);
-        for(auto &x : v)    cin >> x;
-        // cout << minSmoke(v, 0, n - 1) << endl;
-        cout << minSmokeTD(v, 0, n - 1) << endl;
-    }
-    
+    int _t; cin >> _t; while(_t--)
+    solve();
     return 0;
 }
