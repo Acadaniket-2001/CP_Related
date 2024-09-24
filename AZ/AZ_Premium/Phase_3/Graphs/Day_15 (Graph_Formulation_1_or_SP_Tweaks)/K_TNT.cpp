f63453cc04fc1c17a6170ff88444a930d534e536
Aniket Kumar Gupta <135992322+Acadaniket-2001@users.noreply.github.com>
2024-09-24 18:10:34 +0530

    "Day_15 codes updated"

--- AZ/AZ_Premium/Phase_3/Graphs/Day_15 (Graph_Formulation_1_or_SP_Tweaks)/K_TNT.cpp
+++ AZ/AZ_Premium/Phase_3/Graphs/Day_15 (Graph_Formulation_1_or_SP_Tweaks)/K_TNT.cpp
@@ -37,7 +37,7 @@ template <class T>istream& operator >>(istream &is, vector<T> &v) { for(auto &i 
 #ifndef ONLINE_JUDGE
 #define debarr(a,n) cerr<<#a<<" : ";for(int i=0;i<n;i++) cerr<<a[i]<<" "; cerr<<endl;
 #define debmat(mat,row,col) cerr<<#mat<<" :\n";for(int i=0;i<row;i++) {for(int j=0;j<col;j++) cerr<<mat[i][j]<<" ";cerr<<endl;}
-#define debvmat(vec) cerr << #vec << " :\n"; for (auto r: vec) { for (auto c: r) cerr << c << "\t"; cerr << endl; }  // Mine added.
+#define debvmat(vec) cerr << #vec << " :\n"; for (auto r: vec) { for (auto c: r) cerr << c << " "; cerr << endl; }  // Mine added.
 #define pr(...) dbs(#__VA_ARGS__, __VA_ARGS__)
 template <class S, class T>ostream& operator <<(ostream& os, const pair<S, T>& p) {return os << "(" << p.first << ", " << p.second << ")";}
 template <class T>ostream& operator <<(ostream& os, const vector<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
@@ -59,7 +59,7 @@ template <class T>ostream& operator <<(ostream& os, const deque<T> &p){os << "[ 
 #define pr(...){}
 #define debarr(a,n){}
 #define debmat(mat,row,col){}
-#define debvec(vec){}
+#define debvmat(vec){}
 #endif
 //--------------------- //
 long long POW(long long a,long long b){return (long long)(pow(a,b)+0.5);}
@@ -74,141 +74,107 @@ long long Sqrt(long long x){ long long y=sqrt(x)+5;while(y*y>x)y--;return y;}
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
 /*
-Flow:   WAP to print the shortest dist b/w S -> F
-        optimise vis[][] using dis[][]
-        print dis[][]
-        print path S -> F
+Problem: Given K(TNT), find the SP from 'S' -> 'F'
 
-        ..follow_ups..
-        Count all shortest paths S -> F
-        Find shortest path if it is allowed to break atmost K walls.
-        ...
-*/
-
-/*
-6 6
-S . # . . .
+5 6 3
+S # # # # F
+# # # # # .
+# . . # . .
+. . # # # .
 . . # . . .
-# . # . # .
-. . . . # .
-# # # # . .
-F . . . . .
-
-Problem : 1. Find the shortest path from S -> F
-          2. Print the path from S -> F
-          
-          3. Count the number of shortest path from S -> F (Day_15--> Number_of_SP_Using_BFS.cpp)
-          4. Find the minimum of walls needed to break to reach from 'S' -> 'F' (Day_15--> Number_of_Walls_to_Break.cpp)
-          5. Given K(TNT), find the SP from 'S' -> 'F' (Day_15--> K_TNT.cpp)
+o/p: 9
 */
-#define INF 100   // just for this problem
 
+#define INF 1e9
 
+using state = pair<pair<int, int>, int>;     // {{i, j}, left_tnt}
 
-
-int n, m;
-vector<vector<char>> arr;
-using state = pair<int, int>;                                                        // ⭐
-
-// vector<vector<int>> vis;
-vector<vector<int>> dis;                                                             // ⭐ using dis[ ] as vis[ ]  => if dis[i][j] == INF -> (i, j): !vis |||||  only possible in bfs() becoz each node is visited only once in bfs()
-vector<vector<state>> par;                                                           // ⭐ for printing path
+int n, m, k;
+vector<vector<char>> arr; 
+vector<vector<vector<int>>> dis;
 
 bool is_valid(int x, int y) {
-    return (x>=0 and x<n and y>=0 and y<m and arr[x][y] != '#');
+    return (x >= 0 and x < n and y >= 0 and y < m);
 }
 
 int dx[] = {-1, 0, 1, 0};
-int dy[] = {0, 1, 0, -1};                                                            // ⭐ since symmetry about 45° -> shift it by 1/4
+int dy[] = {0, 1, 0, -1};
 
-// int dx[] = {1, 2, 2, 1, -1, -2, -2, -1};
-// int dy[] = {2, 1, -1, -2, -2, -1, 1, 2};
-
-vector<state> neighbours(state node) {
-    vector<state> neighs;
+// ❤️😁😁❤️
+vector<state> neighbours(state node) {      
+    vector<state> neigh;
     f(i, 0, 3) {
-        int x = node.F + dx[i];
-        int y = node.S + dy[i];
+        int x = node.F.F + dx[i];
+        int y = node.F.S + dy[i];
         if(is_valid(x, y)) {
-            neighs.pb({x, y});
+            int z = node.S;
+            if(arr[x][y] == '#' and z > 0) {
+                neigh.pb({{x, y}, z - 1});
+            }
+            else if(arr[x][y] != '#') {
+                neigh.pb({{x, y}, z});
+            }
         }
     }
-    return neighs;
+    return neigh;
 }
 
 void bfs(state st) {
-    // vis.assign(n, vector<int>(m, 0));                                             // ⭐  -> good practice to resize in bfs()
-    dis.assign(n, vector<int>(m, INF));
-    par.assign(n, vector<state>(m, {-1, -1}));
+    dis.assign(n, vector<vector<int>>(m, vector<int>(k + 1, INF)));                      // ⭐ (k+1)=> [0, k]
 
     queue<state> q;
-
-    // vis[st.F][st.S] = 1;
-    dis[st.F][st.S] = 0;
-    par[st.F][st.S] = {-1, -1};
+    dis[st.F.F][st.F.S][st.S] = 0;
     q.push(st);
-    
+
     while(!q.empty()) {
         state node = q.front(); q.pop();
-        for(auto v: neighbours(node)) {
-            // if(!vis[v.F][v.S]) {
-            if(dis[v.F][v.S] == INF) {
+        for(auto v: neighbours(node)) { 
 
-                // vis[v.F][v.S] = 1;
-                dis[v.F][v.S] = dis[node.F][node.S] + 1;
-                par[v.F][v.S] = node;
+            // if(dis[v.F.F][v.F.S][v.S] == INF) {                                              // also works, but only for BFS()
+            if(dis[v.F.F][v.F.S][v.S] > dis[node.F.F][node.F.S][node.S] + 1) {            // ⭐ +1 is for edge cost = #cells reached
+                dis[v.F.F][v.F.S][v.S] = dis[node.F.F][node.F.S][node.S] + 1;
                 q.push(v);
             }
         }
     }
-    
-}
-
-void print_path(state node) {                                                        // ⭐
-    vector<state> path;
-
-    state cur = node;
-    while(cur != mkp(-1, -1)) {
-        path.pb(cur);
-        cur = par[cur.F][cur.S];
-    }
-
-    reverse(all(path));
-    for(auto v: path)   cout << v << endl;
-    ln; 
 }
 
 void solve()
 {
-    cin >> n >> m;
-    state st, en;
+    cin >> n >> m >> k;
+    arr.assign(n, vector<char>(m));
+    pair<int, int>  st, en;
 
-    arr.resize(n);
     f(i, 0, n - 1) {
-        arr[i].resize(m);                                                            // ⭐
-        f(j, 0, m - 1)  {
+        f(j, 0, m - 1) {
             cin >> arr[i][j];
             if(arr[i][j] == 'S')    st = {i, j};
-            if(arr[i][j] == 'F')    en = {i, j};
-        }  
+            else if(arr[i][j] == 'F')    en = {i, j};
+        }
     }
 
-    bfs(st);
-    if(dis[en.F][en.S] != INF) {
-        cout << dis[en.F][en.S] << endl;
-        print_path(en);
+    bfs({st, k});
+    int ans = INF;
+    f(bombs, 0, k) {
+        ans = min(ans, dis[en.F][en.S][bombs]);
     }
+
+    if(ans == INF)
+        cout << "More_💣_Required" << endl;
     else 
-        cout << "Finish is not reachable !!!" << endl;
-
-    debvmat(dis);
+        cout << ans << endl;
 }
 
 int main()
 {
     fastio();
+    // #ifndef ONLINE_JUDGE
+    //     freopen("io/Error.txt", "w", stderr);
+    //     freopen("io/Input.txt", "r", stdin);
+    //     freopen("io/Output.txt", "w", stdout);
+    // #endif
+
+    // int _t; cin >> _t; while(_t--)
     solve();
     return 0;
 }
-
-// T.C. = O(V + E) = O(n*m + 4*(n*m)) = O(n*m);
